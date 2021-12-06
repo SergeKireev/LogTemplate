06c940aa8ca3cea76ed32d25a642efd9bd4531d4
Sergei Kireev <skireev@mediarithmics.com>
2021-12-06 03:11:52 -0500

    Add parsing logic to log ingestion with otlp

--- src/main/scala/io/logtemplate/import/common/FileImportConfig.scala
+++ src/main/scala/io/logtemplate/import/common/FileImportConfig.scala
@@ -23,15 +23,19 @@ sealed trait ImportConfig {
   val config: Config
 }
 
-case class FileImportConfig(config: Config) extends ImportConfig {
-  private def scoped() = config.getConfig("file")
-  def filePath: String = scoped.getOrElse("path", "src/resources/file.log")
+sealed trait RawImportConfig extends ImportConfig {
+  protected def scoped(): Config
   def pattern: DissectPattern = DissectPattern(scoped.getOrElse("dissect.pattern", "%{ts} %{+ts} %{msg}"))
   def dateFormat: String = scoped.getOrElse("date-format", "yyyy-MM-dd hh:mm:ss")
   def multiLineLimit: Int = scoped.getOrElse("multiline.limit", 1000)
 }
 
-case class OpenTelemetryConfig(config: Config) extends ImportConfig {
-  private def scoped() = config.getConfig("opentelemetry")
+case class FileImportConfig(config: Config) extends RawImportConfig {
+  protected def scoped() = config.getConfig("file")
+  def filePath: String = scoped.getOrElse("path", "src/resources/file.log")
+}
+
+case class OpenTelemetryConfig(config: Config) extends RawImportConfig {
+  protected def scoped() = config.getConfig("opentelemetry")
   def port(): Int = scoped().getOrElse("port", 9999)
 }
